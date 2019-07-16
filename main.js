dbc1a3902d93c5b8bd1f4c99fd94001619054d9b
Sid <simsid66@gmail.com>
2019-07-17 02:05:11 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-"use strict";
-
 let Mouse = {
     x: window.innerWidth / 2,
     y: window.innerHeight / 2
@@ -8,23 +6,30 @@ var canvas;
 var renderer;
 var frameCount = 0;
 window.onload = init;
-var runEveryFrame = [];
 
-var entities = []
+var entities = [];
 var player = new Entity(snapToGrid(innerWidth/2),
                         snapToGrid(innerHeight/2),
-                        World.tileSize/2, undefined, '#007cdf', 'rigid', 0);
+                        World.tileSize/2, undefined, '#007cdf', 'rigid', 0, "plr");
 
-var cursor = new Entity(0, 0, 8, undefined, 'red', 'none', 0);
+var cursor = new Entity(0, 0, 8, undefined, 'red', 'none', 0, "crs");
 
-var foo = new Entity(snapToGrid(100), snapToGrid(innerHeight/2), World.tileSize/2, undefined, 'green', 'rigid', 1);
+var foo = new Entity(snapToGrid(100), snapToGrid(innerHeight/2), World.tileSize/2, undefined, 'green', 'rigid', 1, "foo");
+var foo2 = new Entity(snapToGrid(100), snapToGrid(300), World.tileSize/2, undefined, 'green', 'rigid', 1, "foo");
 
 entities.push(player);
 entities.push(foo);
+entities.push(foo2);
 
-function Tile (_x, _y, _side, _image, _colour) {
+/* setInterval(followWrapper, 1000);
 
-}
+function followWrapper() {
+    for(let i = 0; i < entities.length; i++) {
+        if (entities[i].type == 1) {
+            entities[i].follow(player);
+        }
+    }
+} */
 
 function draw() {
     
@@ -32,18 +37,15 @@ function draw() {
     window.requestAnimationFrame(draw);
     renderer.clearRect(0, 0, canvas.width, canvas.height);
     renderer.fillText(frameCount, 10, 10, 100);
-    cursor.x = Mouse.x;
-    cursor.y = Mouse.y;
-    for (let i = 0; i < entities.length; i++) {
-        if (!entities[i].isDead) {
-            entities[i].update();
-            entities[i].draw();
-            entities[i].resolveCollisions();
+    cursor.pos.x = Mouse.x;
+    cursor.pos.y = Mouse.y;
+    
+    for (x of entities) {
+        x.update();
+        if (x) {
+            x.draw();
+            x.resolveCollisions();
         }
-
-        /* if (entities[i].type === 1) {
-            entities[i].follow(player);
-        } */
     }
 
     cursor.draw();
