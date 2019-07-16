398e282d03b1317a0599e8adf3bc9c0a7d5d4f1d
Sid <simsid66@gmail.com>
2019-07-17 02:03:25 +0530

    Update Circle.js

--- Circle.js
+++ Circle.js
@@ -1,16 +1,25 @@
-function Circle(_x, _y, _radius, _colour) {
-    
-    this.radius = _radius;
-    this.x = _x;
-    this.y = _y;
-    this.colour = _colour;
+class Circle {
+    constructor(_x, _y, _radius, _colour) {
+        this.radius = _radius;
+        this.x = _x;
+        this.y = _y;
+        this.colour = _colour;
+    }
 
-    this.draw = function() {
+    fillCircle() {
         renderer.beginPath();
         renderer.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
         renderer.fillStyle = this.colour;
         renderer.fill();
         renderer.closePath();
     }
-
+    
+    strokeCircle(width) {
+        renderer.beginPath();
+        renderer.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        renderer.lineWidth = width;
+        renderer.strokeStyle = this.colour;
+        renderer.stroke();
+        renderer.closePath();
+    }
 }
