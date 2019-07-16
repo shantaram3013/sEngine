e3196f5f210eb131daeb6dc125bc285751bb4295
Sid <simsid66@gmail.com>
2019-07-17 02:02:50 +0530

    Update Entity.js

--- Entity.js
+++ Entity.js
@@ -1,185 +1,166 @@
 "use strict";
 
-function Entity (_x, _y, _colliderRadius, _image, _colour, _colliderType, _type) {
+const States = {
+    IDLE: 0,
+    WALKING: 1,
+    ATTACK1: 2,
+    ATTACK2: 3
+}
 
-    this.colliderRadius = _colliderRadius;
-    this.colour = _colour;
-    this.x = _x;
-    this.y = _y;
-    this.image = _image;
-    this.colliderType = _colliderType;
-    this.type = _type;
-    this.isDead = false;
-    this.health = 100;
-    /*
-     * type 0: cursor/player
-     * type 1: enemy
-     * type 2: bullet
-    */
+class Entity {
 
-    this.oldPos = {
-        x: undefined,
-        y: undefined
+    constructor(_x, _y, _colliderRadius, _image, _colour, _colliderType, _type, id) {
+        this.colliderRadius = _colliderRadius;
+        this.colour = _colour;
+        this.image = _image;
+        this.colliderType = _colliderType;
+        this.type = _type;
+        this.isDead = false;
+        this.State = States.IDLE;
+
+        if (this.type == 0) // TODO: implement separate classes for each
+            this.health = 25;
+        if (this.type == 1)
+            this.health = 15;
+
+        this.timer = 0;
+
+        this.pos = new Vector2(_x, _y);
+
+        this.oldPos = {
+            x: _x,
+            y: _y
+        }
+
+        this.vel = new Vector2(0, 0);
     }
 
-    this.typeSpecificVars = [
-        {}, 
-        {},
-        {
-            destX: 0,
-            destY: 0
-        }
-    ]
-    
-    this.move = function(direction) {
 
+    move(direction) {
         console.log(direction);
-
-        this.oldPos.y = this.y;
-        this.oldPos.x = this.x;
-        
-        switch (direction) {
-            case Directions.UP: for (let i = 0; i < World.tileSize; i++) {
-                                    this.y -= 1;
-                                }
-                                
-                                break;
-            case Directions.DOWN: for (let i = 0; i < World.tileSize; i++) {
-                                    this.y += 1; 
-                                    }
-                                    break;
-            case Directions.LEFT: for (let i = 0; i < World.tileSize; i++) {
-                                    this.x -= 1;
-                                    }
-                                    break;
-            case Directions.RIGHT: for (let i = 0; i < World.tileSize; i++) {
-                                    this.x += 1;
-                                    }
-                                    break;
-        }
-    }
-
-    this.moveAlongVector = function(x1, y1, dist) {
-        
-        var angle = Math.atan2(y1 - this.y, x1 - this.x);
-        var sin = Math.sin(angle) * dist;
-        var cos = Math.cos(angle) * dist;
-
-        if (this.x != x1 || this.y != y1) {
-            this.x += cos;
-            this.y += sin;
-        }
-    }
-    this.update = function() {
-        switch (this.type) {
-            case 2: var dest = {x: this.typeSpecificVars[2].destX, y: this.typeSpecificVars[2].destY};
-                    this.moveAlongVector(dest.x, dest.y, 2);
-                    if (this.x == dest.x && this.y == dest.y ||
-                        Math.floor(this.x) == dest.x && Math.floor(this.y) == dest.y ||
-                        Math.floor(this.x) == dest.x && Math.ceil(this.y) == dest.y ||
-                        Math.ceil(this.x) == dest.x && Math.floor(this.y) == dest.y ||
-                        Math.ceil(this.x) == dest.x && Math.ceil(this.y) == dest.y) this.die();
+        this.oldPos.x = this.pos.x;
+        this.oldPos.y = this.pos.y;
+        if(this.vel != World.maxVel) {
+            switch (direction) {
+                case Directions.UP:
+                    this.vel = this.vel.add(new Vector2(0, -1 * World.step));
                     break;
+                case Directions.DOWN:
+                    this.vel = this.vel.add(new Vector2(0, World.step));
+                    break;
+                case Directions.LEFT:
+                    this.vel = this.vel.add(new Vector2(-1 * World.step, 0));
+                    break;
+                case Directions.RIGHT:
+                    this.vel = this.vel.add(new Vector2(World.step, 0));
+                    break;
+            }
         }
     }
-    this.draw = function() {
-        
-        if (this.isDead) {
-            return;
+
+    update() {
+        if (this.health <= 0) {
+            this.die();
         }
 
+        this.pos = this.pos.add(this.vel);
+        
+        if (this.vel.x > 0) this.vel.x -= World.friction;
+        else if (this.vel.x < 0) this.vel.x += World.friction;
+
+        if (this.vel.y > 0) this.vel.y -= World.friction;
+        else if (this.vel.y < 0) this.vel.y += World.friction;
+
+        if (this.vel.x > 0 && this.vel.x <= World.friction || this.vel.x >= -World.friction && this.vel.x < 0) {
+            this.vel.x = 0;
+        }
+        
+        if (this.vel.y > 0 && this.vel.y <= World.friction || this.vel.y >= -World.friction && this.vel.y < 0) {
+            this.vel.y = 0;
+        }
+    }
+
+    draw() {
+        if (this.isDead)
+            return;
         else {
-            if (typeof this.image == 'undefined') {
-                new Circle(this.x + this.colliderRadius, this.y + this.colliderRadius, this.colliderRadius, this.colour).draw();
+            if (this.image) {
             }
             else {
-                console.log("else block called in this.draw()");
+                new Circle(this.pos.x + this.colliderRadius, this.pos.y + this.colliderRadius, this.colliderRadius, this.colour).fillCircle();
             }
         }
     }
 
-    this.resolveCollisions = function() {
-        let i = 0;
-        
-        for (i = 0; i < entities.length; i++) {
-            
-            if (this === entities[i]) return;
-
-            if (this.colliderType === "none" || entities[i].colliderType === "none") continue; 
-
-            if (entities[i].type == 2|| this.type == 2) {
-                this.bulletCollision(entities[i]);
+    resolveCollisions() {
+        /* for (let i = 0; i < entities.length; i++) {
+            if (this === entities[i])
+                continue;
+            if (this.colliderType === "none" || entities[i].colliderType === "none")
+                continue;
+            if (this.colliderType === "melee" || entities[i].colliderType === "melee") {
+                console.log("Melee collision");
                 continue;
             }
-
-            if(areColliding(this, entities[i])) {
-                    this.x = this.oldPos.x;
-                    this.y = this.oldPos.y;
-
-                    var angle = (Math.atan2(entities[i].y - this.y, entities[i].x - this.x));
-                    /*
-                     * Pi: to the right
-                     * 0: to the left
-                     * Pi/2: on top
-                     * -Pi/2: below
-                    */
+            if (this.isColliding(entities[i])) {
+                this.x = this.oldPos.x;
+                this.y = this.oldPos.y;
             }
-
-            else continue;
-        }
+        } */
     }
 
-    this.ranged = function () {
-        console.log("FIRE!");
-        var projectile = new Entity(this.x + this.colliderRadius, this.y + this.colliderRadius, 3, undefined, 'red', "bullet", 2);
-
-        projectile.typeSpecificVars[2].destX = cursor.x;
-        projectile.typeSpecificVars[2].destY = cursor.y;
-
-        entities.push(projectile);
-       
+    isColliding(x) {
+        if (distance(this.pos.x, x.pos.x, this.pos.y, x.pos.y) < this.colliderRadius + x.colliderRadius)
+            return true;
+        else
+            return false;
     }
 
-    this.bulletCollision = function(cWith) {
-
+    ranged() {
+        if (this.isDead)
+            return;
     }
 
-    this.die = function() {
-        
-        for( var i = 0; i < entities.length; i++){ 
-            if ( entities[i] === this) {
+    melee() {
+        if (this.isDead)
+            return;
+        entities.push(new Melee(5, this, undefined));
+    }
+
+    die() {
+        for (let i = 0; i < entities.length; i++) {
+            if (entities[i] === this) {
                 entities.splice(i, 1);
                 break;
             }
         }
- 
+        console.log("Killing...");
         this.isDead = true;
     }
 
-    /* this.follow = function (subject) {
-        moves = calculateMoves(subject, this);
-        
-        if (frameCount % World.frameRate == 0) {
-            if (Math.sign(moves.x) == 1) {
-                if (Math.abs(deltaX(subject, this)) <= World.tileSize*2) {
-                    this.move(Directions.RIGHT);
-                }
+    /* calculateMovesTo(e2) {
+        return [Math.sign(deltaX(this, e2)) * Math.floor(Math.abs(deltaX(this, e2) / World.tileSize)),
+        Math.sign(deltaY(this, e2)) * Math.floor(Math.abs(deltaY(this, e2) / World.tileSize))];
+    }
+
+    follow(e2) {
+        if (this.isDead)
+            return;
+        let moves = this.calculateMovesTo(e2);
+        if (moves[0] >= 2) {
+            if (Math.sign(moves[0] == 1)) {
+                this.move(Directions.LEFT);
             }
             else {
-                if (Math.abs(deltaX(subject, this)) <= World.tileSize*2) {
-                    this.move(Directions.LEFT);
-                }
+                this.move(Directions.RIGHT);
             }
-            
+        }
+        if (moves[1] >= 2) {
             if (Math.sign(moves[1]) == 1) {
-                if (Math.abs(deltaY(subject, this)) >= World.tileSize*2) {
-                    this.move("down");
-                }
+                this.move(Directions.DOWN);
             }
             else {
-                if (Math.abs(deltaY(subject, this)) >= World.tileSize*2) {
-                    this.move("up");
-                }
+                this.move(Directions.UP);
             }
         }
     } */
